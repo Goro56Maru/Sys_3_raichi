fed875c68170482181f9071c339bb0131bdf1747
Goro56Maru <93966202+Goro56Maru@users.noreply.github.com>
2022-05-17 10:05:14 +0900

    Initial commit

---
+++ app/src/main/java/ecc_sys3_raichi/sys_3_raichi/MainActivity.kt
@@ -0,0 +1,11 @@
+package ecc_sys3_raichi.sys_3_raichi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
