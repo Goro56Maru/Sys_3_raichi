0896d291e0d727734e668503798f713c0812b51f
kzk <gvb>
2022-05-20 13:19:19 +0900

    設定画面

--- app/src/main/java/ecc_sys3_raichi/sys_3_raichi/MainActivity.kt
+++ app/src/main/java/ecc_sys3_raichi/sys_3_raichi/MainActivity.kt
@@ -2,10 +2,22 @@ package ecc_sys3_raichi.sys_3_raichi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        settingsButton.setOnClickListener{
+            replaceFragment(Settings())
+        }
+    }
+    fun replaceFragment(fragment : Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer,fragment)
+            .commit()
     }
 }
