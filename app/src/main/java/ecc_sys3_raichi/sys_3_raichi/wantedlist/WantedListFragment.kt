2f59c6aad3742213bff2ce9a9c76ca4cb939d12b
Goro56Maru <93966202+Goro56Maru@users.noreply.github.com>
2022-05-20 15:02:55 +0900

    WantedList

---
+++ app/src/main/java/ecc_sys3_raichi/sys_3_raichi/wantedlist/WantedListFragment.kt
@@ -0,0 +1,69 @@
+package ecc_sys3_raichi.sys_3_raichi.wantedlist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import ecc_sys3_raichi.sys_3_raichi.databinding.FragmentWantedListBinding
+
+class WantedListFragment : Fragment() {
+
+    //binding
+    private var _binding: FragmentWantedListBinding? = null
+    private val binding get() = _binding!!
+
+    //表示するリストデータ
+    private var ListView : MutableList<WantedListData> = ArrayList()
+    //RecyclerView用のアダプター
+    private val adapter = WantedListAdapter(ListView)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentWantedListBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val linearLayoutManager = LinearLayoutManager(view.context)
+        binding.recyclerView.layoutManager = linearLayoutManager
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.setHasFixedSize(true)
+
+
+        //テスト用データ
+        val listdata2 = arrayOf(arrayOf("id1","テストデータ1","ちち"),arrayOf("id2","テストデータ2","はは"),arrayOf("id3","テストデータ3","ちち"),arrayOf("id4","テストデータ4","ちち"),arrayOf("id5","テストデータ5","がき"))
+
+        ListView.clear()    //リスト表示前にデータを空にしておく
+        for (i in 0..4){
+            //セットするデータ
+            var setList = WantedListData(listdata2[i][0],listdata2[i][1],(i+1)*1000,listdata2[i][2],(i+1))
+            ListView.add(setList)   //リストにデータを追加
+            adapter.notifyDataSetChanged()  //データを追加後、表示を更新
+        }
+
+        adapter.setOnItemClickListener(object :WantedListAdapter.OnItemClickListener{
+
+        })
+
+
+        return view
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            WantedListFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
