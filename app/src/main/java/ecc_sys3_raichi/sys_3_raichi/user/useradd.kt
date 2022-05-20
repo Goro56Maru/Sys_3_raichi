1c5f0be93c94f64af9c3c35e2a584996dc3b0b40
meru <angler_yuu@xmail.plala.or.jp>
2022-05-20 16:12:26 +0900

    useradd usercheck base layout

---
+++ app/src/main/java/ecc_sys3_raichi/sys_3_raichi/user/useradd.kt
@@ -0,0 +1,12 @@
+package ecc_sys3_raichi.sys_3_raichi.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ecc_sys3_raichi.sys_3_raichi.R
+
+class useradd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_useradd)
+    }
+}
