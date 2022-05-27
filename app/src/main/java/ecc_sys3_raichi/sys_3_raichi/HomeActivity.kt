cddf5e94fbb66da5901264fbb04c951c0b2601bb
Goro56Maru <93966202+Goro56Maru@users.noreply.github.com>
2022-05-27 15:38:59 +0900

    aaa

--- app/src/main/java/ecc_sys3_raichi/sys_3_raichi/HomeActivity.kt
+++ app/src/main/java/ecc_sys3_raichi/sys_3_raichi/HomeActivity.kt
@@ -2,10 +2,16 @@ package ecc_sys3_raichi.sys_3_raichi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ecc_sys3_raichi.sys_3_raichi.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
