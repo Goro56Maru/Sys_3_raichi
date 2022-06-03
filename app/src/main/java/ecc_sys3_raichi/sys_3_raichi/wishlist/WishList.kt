c80a100851de69c3a2ddc5a3f05985a574ab0cc4
Goro56Maru <93966202+Goro56Maru@users.noreply.github.com>
2022-06-03 16:05:55 +0900

    欲しいもの登録時のエラー回避処理

--- app/src/main/java/ecc_sys3_raichi/sys_3_raichi/wishlist/WishList.kt
+++ app/src/main/java/ecc_sys3_raichi/sys_3_raichi/wishlist/WishList.kt
@@ -66,7 +66,7 @@ class WishList : Fragment() {
 
             //入力された各情報を変数に入れる
             list_name = binding.wishname.text.toString()
-            //エラー回避のため、何も入力されていない時は0を格納する
+            //エラー回避のため、何も入力されていない時は0を格納
             if (binding.wishmoney.text.isNullOrEmpty()) list_money = 0
             else list_money = binding.wishmoney.text.toString().toInt()
             list_comment = binding.wishcomment.text.toString()
