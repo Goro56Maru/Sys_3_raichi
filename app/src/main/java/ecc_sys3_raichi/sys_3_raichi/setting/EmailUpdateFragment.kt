cf41b100c1302b51a60f5ec11838488f81f68f63
Goro56Maru <93966202+Goro56Maru@users.noreply.github.com>
2022-06-14 11:44:43 +0900

    AccountSetting

--- app/src/main/java/ecc_sys3_raichi/sys_3_raichi/setting/EmailUpdateFragment.kt
+++ app/src/main/java/ecc_sys3_raichi/sys_3_raichi/setting/EmailUpdateFragment.kt
@@ -5,28 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import ecc_sys3_raichi.sys_3_raichi.R
+import ecc_sys3_raichi.sys_3_raichi.databinding.ActivityWishListBinding
+import ecc_sys3_raichi.sys_3_raichi.databinding.FragmentEmailUpdateBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [EmailUpdateFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EmailUpdateFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentEmailUpdateBinding? = null
+    private val binding get() = _binding!!
+
+    //auth
+    private lateinit var auth: FirebaseAuth
+
+    //ログインユーザーのID
+    private var uid = ""
+    //現在のメールアドレス
+    private var old_email = ""
+    //新しいメールアドレス
+    private var new_email = ""
+    //再入力した新しいメールアドレス
+    private var again_new_email = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -34,27 +40,24 @@ class EmailUpdateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_email_update, container, false)
+        _binding = FragmentEmailUpdateBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        //ログインユーザーのIDを取得する
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            uid = auth.uid.toString()
+        }
+
+
+        binding.updateButton.setOnClickListener {
+
+        }
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EmailUpdateFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EmailUpdateFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
