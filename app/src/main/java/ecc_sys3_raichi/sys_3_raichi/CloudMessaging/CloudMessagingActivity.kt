b449c9ec716131b0e7a92f572a77832dace262f5
Goro56Maru <93966202+Goro56Maru@users.noreply.github.com>
2022-07-08 16:04:10 +0900

    CloudMessaging

--- app/src/main/java/ecc_sys3_raichi/sys_3_raichi/CloudMessaging/CloudMessagingActivity.kt
+++ app/src/main/java/ecc_sys3_raichi/sys_3_raichi/CloudMessaging/CloudMessagingActivity.kt
@@ -1,6 +1,7 @@
 package ecc_sys3_raichi.sys_3_raichi.CloudMessaging
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 import ecc_sys3_raichi.sys_3_raichi.R
+import ecc_sys3_raichi.sys_3_raichi.SplashActivity
 import timber.log.Timber
 
 class CloudMessagingActivity : AppCompatActivity() {
@@ -21,6 +23,7 @@ class CloudMessagingActivity : AppCompatActivity() {
 
         // Current Notificationトークンの取得
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+
             if (!task.isSuccessful) {
                 Log.w(TAG, "Fetching FCM registration token failed", task.exception)
                 return@OnCompleteListener
@@ -28,13 +31,14 @@ class CloudMessagingActivity : AppCompatActivity() {
 
 
             // new Instance ID token
-                // ここで取得したtokenをテストする際のインスタンスIDとして設定する
-                val token = task.result
+            // ここで取得したtokenをテストする際のインスタンスIDとして設定する
+            val token = task.result
 
-                val msg = "InstanceID Token: $token"
-                Timber.d(msg)
-                //Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-            })
+            val msg = "InstanceID Token: $token"
+            Timber.d(msg)
+            //Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show(
+            startActivity(Intent(this, SplashActivity::class.java))
+        })
 
     }
 }
